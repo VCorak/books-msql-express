2f57f78c479e16a02ce078691a2fc39c085e8d82
Valentina Corak <valentinacorak@gmail.com>
2021-07-20 15:13:35 +0200

    Bridging database and route; update readme

--- services/books.js
+++ services/books.js
@@ -6,3 +6,24 @@
 // POST /programming-languages → create()
 // PUT /programming-languages/:id → update()
 // DELETE /programming-languages/:id → remove()
+
+const db = require('./db');
+const helper = require('../helper');
+const config = require('../config');
+
+async function getMultiple(page = 1) {
+    const offset = helper.getOffset(page, config.listPerPage);
+    const rows = await db.query(`SELECT id, name, author, genre, review FROM books LIMIT ?,?`,
+        [offset, config.listPerPage]);
+
+    const data = helper.emptyOrRows(rows);
+    const meta = {page};
+
+    return {
+        data, meta
+    }
+}
+
+module.exports = {
+    getMultiple
+}
